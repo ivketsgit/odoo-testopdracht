745aff17c07fdcd3b2abc2fa420b86784b748df0
ivkets <ian.vankets@UGent.be>
2026-05-19 21:23:35 +0200

    [ADD] estate: create base module structure

---
+++ tutorials/estate/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Real Estate',
+    'version': '18.0.1.0.0',
+    'depends': ['base'],
+    'author': 'Ian Van Kets',
+    'category': 'Real Estate',
+    'license': 'LGPL-3',
+    'description': """
+    Module for managing real estate properties, including property name, the property type, the postcode and so on. 
+    """,
+}
