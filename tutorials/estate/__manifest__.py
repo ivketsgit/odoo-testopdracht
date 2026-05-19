47a83aee974aef11f64f688a228b374e80b5489b
ivkets <ian.vankets@UGent.be>
2026-05-19 21:26:47 +0200

    [IMP] real_estate: add application flag to manifest

--- tutorials/estate/__manifest__.py
+++ tutorials/estate/__manifest__.py
@@ -5,6 +5,7 @@
     'author': 'Ian Van Kets',
     'category': 'Real Estate',
     'license': 'LGPL-3',
+    'application': True,
     'description': """
     Module for managing real estate properties, including property name, the property type, the postcode and so on. 
     """,
